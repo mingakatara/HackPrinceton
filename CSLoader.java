180297fd8e4c6e1c13f10ace40e1f4c5985b53db
Gigarion <cdgum@princeton.edu>
2015-11-14 11:06:52 -0500

    Updates

--- CSLoader.java
+++ CSLoader.java
@@ -1,9 +1,27 @@
 // Goes through the game file heirarchy and loads actor information
 // into the engine
+import java.io.*;
+import java.util.Scanner;
 public class CSLoader {
+    private static final String build = "build";
+    private static final String player = "build/player.txt";
+    private File buildFolder;
+    private File playerFile;
+
     CSEngine engine;
 
-    public CSLoader(CSEngine engine) {
-        this.engine = engine;
+    public CSLoader() throws IOException {
+        //this.engine = engine;
+        this.buildFolder = new File(build);
+        this.playerFile = new File(player);
+        Scanner readMe = new Scanner(playerFile);
+        while (readMe.hasNext())
+            System.out.println(readMe.next());
+    }
+
+    // test main
+    public static void main(String[] args) throws IOException {
+
+        CSLoader loader = new CSLoader();
     }
 }
