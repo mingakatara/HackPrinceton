86236cb84cccfcd436633d22677c4cb9acd69bd6
Gigarion <cdgum@princeton.edu>
2015-11-14 00:18:23 -0500

    Updated Actor class
    Currently has full pos/vel/accel set/gets

--- Actor.java
+++ Actor.java
@@ -1,8 +1,18 @@
 
 public class Actor {
-    private double x; // x coord of this actor on the screen
-    private double y; // y coord of this actor on the screen
+
+    /*****************************   Variables   ***********************************/
+    private double x;   // x coord of this actor on the screen
+    private double vx;  // current x velocity of this Actor 
+    private double ax;  // current x acceleration of this Actor
+
+    private double y;  // y coord of this actor on the screen
+    private double vy; // current y velocity of this Actor
+    private double ay  // current y acceleration of this Actor
+
     private String imgName; // image associated with this actor
+
+    /*****************************   Constructors *********************************/
 
     // empty constructor
     public Actor() {
@@ -18,17 +28,74 @@ public class Actor {
         this.imgName = imgName;
     }
 
-    // returns the x location of this Actor
-    public double getX() {
-        return x;
+    /************************ Position/Velocity/Accel set/gets ********************/
+    {
+        // set x coord of this Actor
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        // set y coord of this Actor
+        public void setY(double y) {
+            this.y = y;
+        }
+
+        // set x velocity of this Actor
+        public void setVX(double vx) {
+            this.vx = vx;
+        }
+
+        // set y velocity of this Actor
+        public void setVY(double vy) {
+            this.vy = vy;
+        }
+
+        // set x accel of this Actor
+        public void setAX(double ax) {
+            this.ax = ax;
+        }
+
+        // set y accel of this Actor
+        public void setAY(double ay) {
+            this.ay = ay;
+        }
+
+        //***************** getters
+
+        // returns the x location of this Actor
+        public double getX() {
+            return x;
+        }
+
+        // returns the y location of this Actor
+        public double getY() {
+            return y;
+        }   
+
+        public double getVX() {
+            return vx;
+        }
+
+        // returns the y location of this Actor
+        public double getVY() {
+            return vy
+        }
+
+        public double getAX() {
+            return ax;
+        }
+
+        // returns the y location of this Actor
+        public double getAY() {
+            return ay;
+        }
     }
 
-    // returns the y location of this Actor
-    public double getY() {
+    public void update() {
+        x += vx;
+        vx += ax;
 
-    }
-
-    public void draw() {
-        StdDraw.image(x, y, imgName);
+        y += vy;
+        vy += ay;
     }
 }
