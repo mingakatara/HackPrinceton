50718bd26e233bfd45c16b5acb7ad37eed7c66fd
Gigarion <cdgum@princeton.edu>
2015-11-14 10:47:01 -0500

    Adding CSLoader class
    yay file reading round like 8 or something

---
+++ CSLoader.java
@@ -0,0 +1,9 @@
+// Goes through the game file heirarchy and loads actor information
+// into the engine
+public class CSLoader {
+    CSEngine engine;
+
+    public CSLoader(CSEngine engine) {
+        this.engine = engine;
+    }
+}
