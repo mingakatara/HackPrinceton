bab982fb2ad31a14d836cadb5c6395e3b5898489
Gigarion <cdgum@princeton.edu>
2015-11-14 00:41:50 -0500

    Mad actor updates

--- Actor.java
+++ Actor.java
@@ -8,7 +8,7 @@ public class Actor {
 
     private double y;  // y coord of this actor on the screen
     private double vy; // current y velocity of this Actor
-    private double ay  // current y acceleration of this Actor
+    private double ay; // current y acceleration of this Actor
 
     private String imgName; // image associated with this actor
 
@@ -29,68 +29,66 @@ public class Actor {
     }
 
     /************************ Position/Velocity/Accel set/gets ********************/
-    {
-        // set x coord of this Actor
-        public void setX(double x) {
-            this.x = x;
-        }
-
-        // set y coord of this Actor
-        public void setY(double y) {
-            this.y = y;
-        }
-
-        // set x velocity of this Actor
-        public void setVX(double vx) {
-            this.vx = vx;
-        }
-
-        // set y velocity of this Actor
-        public void setVY(double vy) {
-            this.vy = vy;
-        }
-<<<<<<< HEAD
-
-        // set x accel of this Actor
-        public void setAX(double ax) {
-            this.ax = ax;
-        }
-
-        // set y accel of this Actor
-        public void setAY(double ay) {
-            this.ay = ay;
-        }
-
-        //***************** getters
-
-        // returns the x location of this Actor
-        public double getX() {
-            return x;
-        }
-
-        // returns the y location of this Actor
-        public double getY() {
-            return y;
-        }   
-
-        public double getVX() {
-            return vx;
-        }
-
-        // returns the y location of this Actor
-        public double getVY() {
-            return vy
-        }
-
-        public double getAX() {
-            return ax;
-        }
-
-        // returns the y location of this Actor
-        public double getAY() {
-            return ay;
-        }
+    // set x coord of this Actor
+    public void setX(double x) {
+        this.x = x;
     }
+
+    // set y coord of this Actor
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    // set x velocity of this Actor
+    public void setVX(double vx) {
+        this.vx = vx;
+    }
+
+    // set y velocity of this Actor
+    public void setVY(double vy) {
+        this.vy = vy;
+    }
+
+    // set x accel of this Actor
+    public void setAX(double ax) {
+        this.ax = ax;
+    }
+
+    // set y accel of this Actor
+    public void setAY(double ay) {
+        this.ay = ay;
+    }
+
+    //***************** getters
+
+    // returns the x location of this Actor
+    public double getX() {
+        return x;
+    }
+
+        // returns the y location of this Actor
+    public double getY() {
+        return y;
+    }   
+
+    public double getVX() {
+        return vx;
+    }
+
+        // returns the y location of this Actor
+    public double getVY() {
+        return vy;
+    }
+
+    public double getAX() {
+        return ax;
+    }
+
+        // returns the y location of this Actor
+    public double getAY() {
+        return ay;
+    }
+
 
     // set the filepath for this Actor's image
     public void setImgName(String imgName) {
@@ -98,50 +96,8 @@ public class Actor {
     }
 
     // returns the filepath for this Actor's Image
-    public void getImgName() {
+    public String getImgName() {
         return this.imgName;
-=======
-
-        // set x accel of this Actor
-        public void setAX(double ax) {
-            this.ax = ax;
-        }
-
-        // set y accel of this Actor
-        public void setAY(double ay) {
-            this.ay = ay;
-        }
-
-        //***************** getters
-
-        // returns the x location of this Actor
-        public double getX() {
-            return x;
-        }
-
-        // returns the y location of this Actor
-        public double getY() {
-            return y;
-        }   
-
-        public double getVX() {
-            return vx;
-        }
-
-        // returns the y location of this Actor
-        public double getVY() {
-            return vy
-        }
-
-        public double getAX() {
-            return ax;
-        }
-
-        // returns the y location of this Actor
-        public double getAY() {
-            return ay;
-        }
->>>>>>> origin/master
     }
 
     public void update() {
